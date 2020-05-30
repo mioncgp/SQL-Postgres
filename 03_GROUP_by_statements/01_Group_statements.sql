2c3ef2ce651747929df0a2e3a77b3b55791990da
Michael <mioncgp@gmail.com>
2020-05-30 18:25:02 +0400

    d

---
+++ 03_GROUP_by_statements/01_Group_statements.sql
@@ -0,0 +1,6 @@
+-- The MIN() function returns the smallest value of the selected column.
+-- The MAX() function returns the largest value of the selected column.
+-- The AVG() function returns the average value of a numeric colum
+-- The SUM() function returns the total sum of a numeric column.
+-- ROUND rounds off a number, the first arg a number the second arg how many decimals
+
