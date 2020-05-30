d03cc2e36cf4fd5c8fb0df4ce177d6e3801f5a17
Michael <mioncgp@gmail.com>
2020-05-30 15:30:26 +0400

    d

--- 1 SQL Fundamentals/02_select_statement.sql
+++ 1 SQL Fundamentals/02_select_statement.sql
@@ -17,3 +17,12 @@
 --  != Not equal
 -- AND
 -- OR
+-- The COUNT function returns the number of input rows that match a specific condition of a query
+-- LIMIT allows you to limit the number of rows you get back after a query
+-- The ORDER BY keyword sorts the records in ascending order by default. To sort the records in descending order, use the DESC keyword.
+-- The BETWEEN operator selects values within a given range. The values can be numbers, text, or dates. The BETWEEN operator is inclusive: begin and end values are included.
+-- The IN operator allows you to specify multiple values in a WHERE clause. The IN operator is a shorthand for multiple OR condition
+-- The LIKE operator is used in a WHERE clause to search for a specified pattern in a column.
+-- Percent(%) for matching any sequence of characters
+-- Underscore (_) for matching any single character
+-- LIKE by itself is case sensative, in the meanwhile ILIKE is not
