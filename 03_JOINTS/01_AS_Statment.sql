37e6023fbf8dcd2e0db2cb93ebe8604d9ac6c3c1
Michael <mioncgp@gmail.com>
2020-06-02 17:24:00 +0400

    d

--- 03_JOINTS/01_AS_Statment.sql
+++ 03_JOINTS/01_AS_Statment.sql
@@ -1,3 +1,13 @@
 -- AS allows us to rename columns or table selections with an alias
 
 -- A JOIN clause is used to combine rows from two or more tables, based on a related column between them.
+-- The INNER JOIN clause returns rows in A table that have the corresponding rows in the B table
+
+-- FULL OUTER JOIN: produces the set of all records in Table A and Table B, with matching records from
+-- both sides where avaliable.If there is no match, the missing side will contain null
+
+-- LEFT OUTER JOIN: produces a complete set of records from Table A, with the matching records.
+-- LEFT OUTER JOIN with WHERE: to produce the set of records only in Table A, but not in Table B.
+
+-- FULL OUTER JOIN with WHERE: to produce the set of records unique to Table A and Table B, 
+-- we perform the same full outer join, then exclude the records we don't want from both sides via a WHERE clause
